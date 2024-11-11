5750f4f15a4aa978b1f27cc2ebc1d42d1d3c605d
Suraj Singh <surajpratapsingh9798@gmail.com>
2024-11-12 01:29:32 +0530

    Files_uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-// import AboutMe from "./components/AboutMe";
 import Designing from "./components/Designing";
-// import DigitalMaster from "./components/DigitalMaster";
 import Footer from "./components/Footer";
-// import GetToKnow from "./components/GetToKnow";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-// import Transform from "./components/Transform";
-
 export default function App() {
   return (
     <>
@@ -17,8 +12,6 @@ export default function App() {
       <Designing />
       <Skills />
       <Projects />
-      {/* <Transform /> */}
-      {/* <DigitalMaster /> */}
       <Footer />
     </>
   )
